26cb4305ab5bc8bd6781d7a08fe199c5f8ce867c
dragon0924 <lexus_430s@yahoo.com>
2020-07-16 01:06:17 -0700

    Update Exercise3.c

--- Exercise3.c
+++ Exercise3.c
@@ -11,10 +11,21 @@ ______________________________________
 
 int main(int argc, char *argv[]) {
 	//testing variable, applying it to your algorithm for auto-evaluating
-    int testcase = atoi(argv[1]);
+	int testcase = atoi(argv[1]);
 	
 	//your codes here
-	
+	int i, n,h=0;
+	n = testcase;
+    	for (i=2;i<=n;i++)
+	{
+		while (n%i==0)
+		{
+			if (h==0) printf("%d",i);
+			else printf("%d",i);
+			n/=i;
+			h++;
+		}
+	}
 	
 	return 0;
 }
